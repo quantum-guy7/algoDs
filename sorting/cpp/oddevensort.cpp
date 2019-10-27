fde72eeeb89fccc00e6c009350177779ea425306
suvidsahay <sahay.suvid61@gmail.com>
2019-10-27 17:06:55 +0530

    Odd Even Sort

--- sorting/cpp/oddevensort.cpp
+++ sorting/cpp/oddevensort.cpp
@@ -14,12 +14,14 @@ void oddEvenSort(int arr[], int n)
     bool sorted = false;
     while(!sorted) {
         sorted = true;
+	// bubble sort on odd indexed element
         for(int i = 1; i < n-1; i += 2){
             if(arr[i] > arr[i+1]) {
                 swap(&arr[i], &arr[i+1]);
                 sorted = false;
             }
         }
+	// bubble sort on even indexed element
         for(int i = 0; i < n-1; i += 2){
             if(arr[i] > arr[i+1]) {
                 swap(&arr[i], &arr[i+1]);
